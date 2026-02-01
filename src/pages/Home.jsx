22be2b3e1f851bd77f0fe8611f85a5b13f565b9d
karismita_27 <karismitaa27@gmail.com>
2026-02-01 22:19:47 +0530

    promise section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -177,9 +177,9 @@ export default function Home() {
       <div className="max-w-6xl mx-auto px-4">
         <h2 className="text-center font-semibold text-xl mb-6">Our Promises</h2>
 
-        <div className="grid grid-cols-4 gap-4 place-items-center">
+        <div className="flex sm:grid gap-4 pb-4 overflow-x-auto sm:overflow-visible sm:grid-cols-2 lg:grid-cols-4">
           {promises.map((p) => (
-            <div key={p.title} className="flex flex-col items-center text-center bg-white border border-gray-200 rounded-xl p-4 shadow-sm w-48 h-36">
+            <div key={p.title} className="min-w-[26px] sm:min-w-0 flex flex-col items-center text-center bg-white border border-gray-200 rounded-xl p-4 shadow-sm w-48 h-36">
               <div className="w-10 h-10 bg-yellow-200 text-yellow-700 rounded-lg flex items-center justify-center text-sm">
                 <PromiseIcon type = {p.type} />
               </div>
